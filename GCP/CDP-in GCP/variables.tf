6ceee87e8f769cfdf7dd7e5ac49de2b00e65a16e
Yuriy <p.yuriy@gmail.com>
2026-02-17 14:22:08 -0500

    Add new variables for project configuration

--- GCP/CDP-in GCP/variables.tf
+++ GCP/CDP-in GCP/variables.tf
@@ -1,9 +1,32 @@
-variable "ssh_pub_key_path" {
-  description = "Path to the SSH public key"
-  default     = "~/.ssh/id_rsa.pub"
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
 }
 
 variable "ssh_user" {
-  description = "SSH user to create"
+  description = "The user that will be created on the VMs"
   default     = "centos"
+}
+
+variable "ssh_pub_key_path" {
+  description = "Local path to your public SSH key"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "node_names" {
+  type    = list(string)
+  default = ["master-01", "worker-01", "worker-02"]
+}
+
+variable "machine_type" {
+  description = "Size of the VMs"
+  default     = "n2-standard-8"
 }
